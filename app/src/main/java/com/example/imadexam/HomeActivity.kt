4b0f1572e4f68be40ffb9e0889e558e8ec797281
MehaanSelvanPillay911 <St10537135@myemeris.edu.za>
2026-06-10 11:27:22 +0200

    var + space added
    var declared for home act

--- app/src/main/java/com/example/imadexam/HomeActivity.kt
+++ app/src/main/java/com/example/imadexam/HomeActivity.kt
@@ -1,16 +1,42 @@
 package com.example.imadexam
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var tvList: TextView
+    private lateinit var edtGear: EditText
+    private lateinit var btnDark: Button
+    private lateinit var btnAdd: Button
+    private lateinit var btnGear: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
+
+
+
+        
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
