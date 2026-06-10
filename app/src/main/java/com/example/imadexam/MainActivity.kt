7e778586f188fef8ca59e593aca55781affdecc7
MehaanSelvanPillay911 <St10537135@myemeris.edu.za>
2026-06-10 10:35:59 +0200

    Buttons backend code
    The backend code for the exit and enter buttons(1 red line under Enter Button)

--- app/src/main/java/com/example/imadexam/MainActivity.kt
+++ app/src/main/java/com/example/imadexam/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.imadexam
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnEnter: Button
+    private lateinit var btnExit: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //type
+        btnStart = findViewById(R.id.btnEnter)
+        btnExit = findViewById(R.id.btnExit)
+
+        btnStart.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnExit.setOnClickListener {
+            finishAffinity()
+        }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
